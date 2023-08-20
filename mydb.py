98b06afe15d9b2f5b76383e0d4ef3de9b98406de
Mrgudboy666 <pubgshakir@gmail.com>
2023-08-20 23:20:49 +0530

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,24 @@
+# Install Mysql on computer
+# pip install mysql
+# pip install mysql-connector
+# pip install mysql-connector-python
+
+
+
+import mysql.connector
+
+# Connect to the database
+dataBase = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='Mysql123#',
+)
+
+# Prepare a cursor object
+cursorObject = dataBase.cursor()
+
+# Create database
+cursorObject.execute("CREATE DATABASE IF NOT EXISTS salescompany")
+
+print("All done!")
+
